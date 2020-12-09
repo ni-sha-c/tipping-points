5b084995b650268d69c62d64f60b6e950f492938
Nisha Chandramoorthy <nishac@mit.edu>
2020-12-08 20:53:32 -0500

    Fix errors in tangent

--- solenoid.jl
+++ solenoid.jl
@@ -10,6 +10,7 @@ begin
 	using Plots
 	using PlutoUI
 	using LinearAlgebra
+	using Test
 end
 
 # ╔═╡ 7abc4f04-3772-11eb-1c9b-712985ec2af7
@@ -26,36 +27,48 @@ function solenoid(x, s)
 	θ = (atan(x[2],x[1]) + 2π) % (2π) 
 	z = x[3]
 	
-	r = s₀ + (r - s₀)/s₁ + cos(θ)/2
-	θ = 2θ
-	z = z/s₁ + sin(θ)/2
+	r₁ = s₀ + (r - s₀)/s₁ + cos(θ)/2
+	θ₁ = 2θ
+	z₁ = z/s₁ + sin(θ)/2
 	
 	xn = similar(x)
-	xn[1] = r*cos(θ)
-	xn[2] = r*sin(θ)
-	xn[3] = z
+	xn[1] = r₁*cos(θ₁)
+	xn[2] = r₁*sin(θ₁)
+	xn[3] = z₁
 	
 	return xn
 end
 
 # ╔═╡ 9d23a890-3773-11eb-1d5e-295158261eea
 function run(method, x₀, s, n_steps)
-	n = 0
-	orbit = [x₀]
+	n = 1
+	orbit = zeros(3, n_steps)
 	x₁ = copy(x₀)
 	while n < n_steps
 		x₁ = method(x₁, s)
-		push!(orbit, x₁)
+		orbit[:,n] = x₁
 		n += 1
 	end
 	return orbit
 end
 
+# ╔═╡ 86f96d6c-39b6-11eb-3e44-e3a3d68a6957
+function spinup(method, x₀, s, n_steps)
+	n = 1
+	x₁ = similar(x₀)
+	while n < n_steps
+		x₁ = method(x₁, s)
+		n += 1
+	end
+	return x₁
+end
+
 # ╔═╡ afd50d1c-3776-11eb-2696-abce7d038b88
 begin
 	x₀ = rand(3)
-	n_steps = 2000
+	n_steps = 10000
 	s = [1.0, 4.0]
+	x₀ = spinup(solenoid, x₀, s, 1000)
 	orbit = run(solenoid, x₀, s, n_steps)
 end
 
@@ -69,12 +82,25 @@ let
     title = "Solenoid Attractor",
     marker = 2,
 	leg = false,
-	linealpha = 0
+	linealpha = 0,
+	xlabel = "x",
+	ylabel = "y",
+	zlabel = "z"
 )
-@gif for n = 1:1000
-	push!(plt, orbit[n][1], orbit[n][2], orbit[n][3])
+@gif for n = 1:n_steps
+	push!(plt, orbit[1,n], orbit[2,n], orbit[3,n])
 	end every 10
 end
+
+# ╔═╡ a4a93482-39b1-11eb-0753-c5aeb0eda8ae
+let
+	
+	p1 = plot(orbit[1,:], orbit[2,:], m=:o, ms=2, linealpha=0, leg=false, xlabel="x", ylabel="y")
+	p2 = plot(orbit[2,:], orbit[3,:], m=:o, ms=2, linealpha=0, leg=false, xlabel="y", ylabel="z")
+
+	plot(p1, p2, layout = grid(1, 2, widths=[0.5, 0.5]))
+end
+
 
 # ╔═╡ 5720aa0c-38ed-11eb-073e-2d5e0332329d
 md"""
@@ -82,7 +108,7 @@ md"""
 """
 
 # ╔═╡ c619c9e4-390a-11eb-3819-3d17ff4aecd2
-function tangent(x, v)
+function tangent(x, v, s=[1.,4.])
 		r_sq = x[1]*x[1] + x[2]*x[2]
 		r = sqrt(r_sq)
 		θ = (atan(x[2], x[1]) + 2π) % (2π) 
@@ -119,23 +145,34 @@ function tangent(x, v)
 		dy1_dθ = dy1_dr1*dr1_dθ + dy1_dθ1*dθ1_dθ
 		
 		
-		
-		v[1] = dx1_dr*dr_dx*v[1] + 
+		v₁ = similar(v)
+		v₁[1] = dx1_dr*dr_dx*v[1] + 
 			   dx1_dθ*dθ_dx*v[1] + 
 			   dx1_dr*dr_dy*v[2] + 
 			   dx1_dθ*dθ_dy*v[2] 
 			   
 			   
-		v[2] = dy1_dr*dr_dx*v[1] + 
+		v₁[2] = dy1_dr*dr_dx*v[1] + 
 			   dy1_dθ*dθ_dx*v[1] + 
 			   dy1_dr*dr_dy*v[2] + 
 			   dy1_dθ*dθ_dy*v[2] 
 		
-		v[3] = dz1_dθ*dθ_dx*v[1] +
-			   dz1_dθ*dθ_dy*v[2]
+		v₁[3] = dz1_dθ*dθ_dx*v[1] +
+			   dz1_dθ*dθ_dy*v[2] +
 			   dz1_dz*v[3]
-		
-		return v
+		return v₁
+end
+
+# ╔═╡ ca2dbc4e-3972-11eb-15e0-c595f6d2d087
+function test_tangent(x, v, s=[1.,4.],ϵ=1.e-6)
+	fx = solenoid(x, s)
+	df_dx = solenoid(x .+ ϵ*[1., 0., 0.], s) - solenoid(x .- ϵ*[1., 0., 0.], s)
+	df_dy = solenoid(x .+ ϵ*[0., 1., 0.], s) - solenoid(x .- ϵ*[0., 1., 0.], s)
+	df_dz = solenoid(x .+ ϵ*[0., 0., 1.], s) - solenoid(x .- ϵ*[0., 0., 1.], s)
+	v_ana = tangent(x, v)
+	v_fd = Matrix([df_dx df_dy df_dz])*v/(2*ϵ)
+	@show v_ana, v_fd
+	@test v_ana ≈ v_fd atol=1.e-6
 end
 
 # ╔═╡ 7395d3e2-38ed-11eb-37e9-d734cb6fffae
@@ -157,17 +194,38 @@ end
 LE(rand(3), rand(3), [1.4, 1], 3500)
 
 # ╔═╡ 8f58770a-395c-11eb-364d-5b3e302667a1
+begin
+	v₀ = rand(3)
+	#x₀ = rand(3)
+	v = copy(v₀)
+	x = copy(x₀)
+	#n_steps = 100
+	#s = [1.,4.]
+	x = [1.0, 1.0, 1.]
+	v = [1.0, 0.,0.]
+	for n = 1:1
+		v .= tangent(x, v)
+		x .= solenoid(x, s)
+	end
+	
+end
 
+# ╔═╡ bbe580e0-3972-11eb-0fc7-4d95edb00ca0
+test_tangent(rand(3), rand(3))
 
 # ╔═╡ Cell order:
 # ╟─7abc4f04-3772-11eb-1c9b-712985ec2af7
 # ╠═b49dd7c8-3772-11eb-3414-bb1f9e84b748
 # ╠═f8e62384-3772-11eb-23fb-41462d88e988
 # ╠═9d23a890-3773-11eb-1d5e-295158261eea
+# ╠═86f96d6c-39b6-11eb-3e44-e3a3d68a6957
 # ╠═afd50d1c-3776-11eb-2696-abce7d038b88
 # ╠═3a605d5e-3776-11eb-34ff-67296e7637f3
+# ╠═a4a93482-39b1-11eb-0753-c5aeb0eda8ae
 # ╠═5720aa0c-38ed-11eb-073e-2d5e0332329d
 # ╠═c619c9e4-390a-11eb-3819-3d17ff4aecd2
+# ╠═ca2dbc4e-3972-11eb-15e0-c595f6d2d087
 # ╠═7395d3e2-38ed-11eb-37e9-d734cb6fffae
 # ╠═7d245e0e-390d-11eb-28dd-7b1504da3c0f
 # ╠═8f58770a-395c-11eb-364d-5b3e302667a1
+# ╠═bbe580e0-3972-11eb-0fc7-4d95edb00ca0
