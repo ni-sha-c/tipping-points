47ec7956a8fc8fa0ffb5f7323cf6ba5059ed5046
Nisha Chandramoorthy <nishac@mit.edu>
2020-12-10 15:24:24 -0500

    Add random solenoid

--- solenoid.jl
+++ solenoid.jl
@@ -48,6 +48,30 @@ function solenoid(x, s)
 	return xn
 end
 
+# ╔═╡ 1f1702b4-3b22-11eb-2013-d7d91678263e
+function random_solenoid(x, s)
+	
+	s₀, s₁ = s[1], s[2]
+	#s₀ = 2s₀
+	if rand() < 0.5 
+		s₀ = 2s₀
+	end
+	r = sqrt(x[1]*x[1] + x[2]*x[2])
+	θ = (atan(x[2],x[1]) + 2π) % (2π) 
+	z = x[3]
+	
+	r₁ = s₀ + (r - s₀)/s₁ + cos(θ)/2
+	θ₁ = 2θ
+	z₁ = z/s₁ + sin(θ)/2
+	
+	xn = similar(x)
+	xn[1] = r₁*cos(θ₁)
+	xn[2] = r₁*sin(θ₁)
+	xn[3] = z₁
+	
+	return xn
+end
+
 # ╔═╡ 9d23a890-3773-11eb-1d5e-295158261eea
 function run(method, x₀, s, n_steps)
 	n = 1
@@ -64,7 +88,7 @@ end
 # ╔═╡ 86f96d6c-39b6-11eb-3e44-e3a3d68a6957
 function spinup(method, x₀, s, n_steps)
 	n = 1
-	x₁ = similar(x₀)
+	x₁ = copy(x₀)
 	while n < n_steps
 		x₁ = method(x₁, s)
 		n += 1
@@ -75,10 +99,10 @@ end
 # ╔═╡ afd50d1c-3776-11eb-2696-abce7d038b88
 begin
 	x₀ = rand(3)
-	n_steps = 1500
+	n_steps = 5000
 	s = [1.0, 4.0]
-	x₀ = spinup(solenoid, x₀, s, 1000)
-	orbit = run(solenoid, x₀, s, n_steps)
+	x₀ = spinup(random_solenoid, x₀, s, 1000)
+	orbit = run(random_solenoid, x₀, s, n_steps)
 end
 
 # ╔═╡ 3a605d5e-3776-11eb-34ff-67296e7637f3
@@ -190,7 +214,7 @@ function first_LE(x₀, s, n_steps)
 	x = copy(x₀)
 	λ = 0.
 	for n = 1:n_steps
-		v .= tangent(x, v)
+		v .= tangent(x, v, s)
 		α = norm(v)
 		λ += log(α)/n_steps
 		v ./= α
@@ -226,9 +250,9 @@ function all_LEs(x₀, s, n_steps)
 		v₃ .= Q[:,3]
 		
 		
-		v₁ .= tangent(x, v₁)
-		v₂ .= tangent(x, v₂)
-		v₃ .= tangent(x, v₃)
+		v₁ .= tangent(x, v₁, s)
+		v₂ .= tangent(x, v₂, s)
+		v₃ .= tangent(x, v₃, s)
 		x .= solenoid(x, s)
 	end
 	@show α
@@ -256,12 +280,12 @@ md"""
 # ╔═╡ 25ab64f0-3a53-11eb-260d-2d71467086fe
 begin
 	les_1 = zeros(3, n_p)
-	s1 = LinRange(1.0, 2.5, n_p)
+	s1 = LinRange(1.0, 6.5, n_p)
 	for i = 1:n_p
 		les_1[:,i] = compute_LEs([s1[i], 4.0])
 	end
 	les_1 = les_1'
-	p1 = plot(s1, les_1[:,1], m=:o, xlim=(1.0, 2.6), ylim=(0, 0.7), title="1st LE", leg=false)
+	p1 = plot(s1, les_1[:,1], m=:o, title="1st LE", leg=false)
 	p2 = plot(s1, les_1[:,2], m=:o, xlim=(1.0, 2.6), ylim=(-2, -1), title="2nd LE", leg=false)
 	p3 = plot(s1, les_1[:,3], m=:o, xlim=(1.0, 2.6), ylim=(-2, -1), title="3rd LE", leg=false)
 	
@@ -277,15 +301,15 @@ md"""
 
 # ╔═╡ 1c2fb750-3a5d-11eb-347c-49a673d2c39b
 begin
-	s2 = LinRange(2.0, 4.5, n_p)
+	s2 = LinRange(2.0, 10., n_p)
 	les_2 = zeros(n_p, 3)
 	for i = 1:n_p
 		les_2[i,:] = compute_LEs([1.0, s2[i]])
 	end
 	
-	p4 = plot(s2, les_2[:,1], m=:o, xlim=(1.0, 4.6), ylim=(0.5,1.0),title="1st LE", leg=false)
-	p5 = plot(s2, les_2[:,2], m=:o, xlim=(1.0, 4.6), ylim=(-2, -1), title="2nd LE", leg=false)
-	p6 = plot(s2, les_2[:,3], m=:o, xlim=(1.0, 4.6), ylim=(-2, -1), title="3rd LE", leg=false)
+	p4 = plot(s2, les_2[:,1], m=:o,title="1st LE", leg=false)
+	p5 = plot(s2, les_2[:,2], m=:o, title="2nd LE", leg=false)
+	p6 = plot(s2, les_2[:,3], m=:o, title="3rd LE", leg=false)
 	
 	plot(p4, p5, p6, layout = grid(1, 3, widths=[0.3, 0.3, 0.3]))
 end
@@ -322,9 +346,9 @@ begin
 	x₀ .= spinup(solenoid, x₀, [s1[m], 4.0], 5000)
 	orbit_s1 = zeros(n_steps, 3, n_p)
 	orbit_s1[:,:,m] = run(solenoid, x₀, [s1[m], 4.0], n_steps)'
-	p11 = plot(orbit_s1[:,1,m], orbit_s1[:,2,m], m=:o, ms=2, linealpha=0, xlim=(-3.1,3.1), ylim=(-3.1,3.1),leg=false, xlabel="x", ylabel="y")
+	p11 = plot(orbit_s1[:,1,m], orbit_s1[:,2,m], m=:o, ms=2, linealpha=0, xlim=(-7.5,7.5), ylim=(-7.5,7.5),leg=false, xlabel="x", ylabel="y")
 	p12 = plot(orbit_s1[:,2,m], orbit_s1[:,3,m], m=:o, ms=2, linealpha=0,
-		xlim=(-2.5,2.5), ylim=(-1.,1.),leg=false, xlabel="y", ylabel="z")
+		xlim=(-7.5,7.5), ylim=(-1.,1.),leg=false, xlabel="y", ylabel="z")
 
 	plot(p11, p12, layout = grid(1, 2, widths=[0.5, 0.5]))
 end
@@ -364,7 +388,7 @@ function construct_transition_matrix(orbit, n_nodes)
 		ind = ind_x + (ind_y-1)*n_nodes + (ind_z-1)*n_nodes*n_nodes
 		
 		
-		P[pre_ind, ind] += 1
+		P[ind, pre_ind] += 1
 		pre_ind = ind
 	end
 	for i = 1:n_nodes^3
@@ -378,16 +402,30 @@ end
 
 # ╔═╡ 78d0a378-3a76-11eb-038f-5b319336a827
 begin
-	test_orbit = run(solenoid, x₀, s, 10000000)
-	P = construct_transition_matrix(test_orbit, 6);
+	n_nodes = 10
+	P1 = zeros(n_nodes^3, n_nodes^3, n_p)
+	for (i, s1i) in enumerate(s1)
+		@show s1i
+		x = spinup(solenoid, rand(3), [s1i, 4.0], 1000)
+		test_orbit = run(solenoid, x, [s1i, 4.0], 1000000)
+		P1[:,:,i]  = construct_transition_matrix(test_orbit, n_nodes)
+	end
 end
 
 # ╔═╡ 4144af3c-3b07-11eb-028c-6ddd7b0c000d
-rpr = eigvals(P)
+begin
+	rpr1 = 1im*zeros(n_nodes^3, n_p)
+	for i =1:n_p
+		rpr1[:,i] = eigvals(P1[:,:,i])
+	end
+end
+
+# ╔═╡ 84a7b078-3b0f-11eb-1b67-5f903684d8a8
+@bind j Slider(1:n_p, show_value=true)
 
 # ╔═╡ 57453d90-3b06-11eb-1834-19f0f3481337
 begin
-	p13 = plot(real(rpr), imag(rpr), xlim=(-1,1), ylim=(-1,1), linealpha=0, ms=2, m=:o, leg=false, aspect_ratio=1, title="Eigenvalues of Frobenius-Perron operator")
+	p13 = plot(real(rpr1[:,j]), imag(rpr1[:,j]), xlim=(-1,1), ylim=(-1,1), linealpha=0, ms=2, m=:o, leg=false, aspect_ratio=1, title="Eigenvalues of Frobenius-Perron operator")
 	t_arr = 0.:pi/20:2π
 	plot!(p13, cos.(t_arr), sin.(t_arr))
 end
@@ -399,6 +437,7 @@ end
 # ╟─7abc4f04-3772-11eb-1c9b-712985ec2af7
 # ╠═b49dd7c8-3772-11eb-3414-bb1f9e84b748
 # ╠═f8e62384-3772-11eb-23fb-41462d88e988
+# ╠═1f1702b4-3b22-11eb-2013-d7d91678263e
 # ╠═9d23a890-3773-11eb-1d5e-295158261eea
 # ╠═86f96d6c-39b6-11eb-3e44-e3a3d68a6957
 # ╠═afd50d1c-3776-11eb-2696-abce7d038b88
@@ -406,8 +445,8 @@ end
 # ╠═a4a93482-39b1-11eb-0753-c5aeb0eda8ae
 # ╠═5720aa0c-38ed-11eb-073e-2d5e0332329d
 # ╟─c619c9e4-390a-11eb-3819-3d17ff4aecd2
-# ╠═ca2dbc4e-3972-11eb-15e0-c595f6d2d087
-# ╠═7395d3e2-38ed-11eb-37e9-d734cb6fffae
+# ╟─ca2dbc4e-3972-11eb-15e0-c595f6d2d087
+# ╟─7395d3e2-38ed-11eb-37e9-d734cb6fffae
 # ╠═83546b9c-39c2-11eb-0772-ff2a7d6ed0fe
 # ╠═bbe580e0-3972-11eb-0fc7-4d95edb00ca0
 # ╠═204d842c-39ca-11eb-19fe-9fdffed8d7df
@@ -426,5 +465,6 @@ end
 # ╠═d6727a82-3a6a-11eb-0fa9-7f131ee65966
 # ╠═78d0a378-3a76-11eb-038f-5b319336a827
 # ╠═4144af3c-3b07-11eb-028c-6ddd7b0c000d
+# ╠═84a7b078-3b0f-11eb-1b67-5f903684d8a8
 # ╠═57453d90-3b06-11eb-1834-19f0f3481337
 # ╠═9d2331a2-3b07-11eb-3f5f-0bdf673ff93d
